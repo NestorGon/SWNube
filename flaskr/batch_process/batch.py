83037d4752a80eb43ec799ea1babdf1701fe7d85
Nestor Gonzalez <nf.gonzalez@uniandes.edu.co>
2022-10-16 19:39:38 -0500

    Port in task request

--- flaskr/batch_process/batch.py
+++ flaskr/batch_process/batch.py
@@ -7,5 +7,5 @@ celery = Celery('tasks', broker='redis://localhost:6379/0')
 @celery.task(name="start_conversion")
 def start_conversion(task_id,in_route,out_route,in_ext,out_ext):
     AudioSegment.from_file(in_route[1::],format=in_ext).export(out_route[1::], format=out_ext)
-    r = requests.post(f'http://127.0.0.1/api/tasks/{task_id}/processed')
+    r = requests.post(f'http://127.0.0.1:5000/api/tasks/{task_id}/processed')
     print(f'Response: {r.status_code}')
